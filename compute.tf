8946830c783b3ad1d70243e919deba982722e987
Realsammy <realsammy86@gmail.com>
2024-09-16 10:06:31 +0100

    commented out eks provissioning

--- compute.tf
+++ compute.tf
@@ -1,29 +1,29 @@
-#module "cluster_resourece" {
-#    source = "./ekscluster"
+module "cluster_resourece" {
+    source = "./ekscluster"
 
- #   cluster-name   = "myNewCluster"
-  #  region         = "us-east-1" 
-  #  instance-types = [ "t2.micro" ] 
-  #  desired_size   = 1
-  #  max_size       = 2
-  #  min_size       = 1
-   # max_unaval     = 1
-   # cidr_block     = "10.0.0.0/16"
-   # dns_enable     = true
-   # dns_host       = true
+    cluster-name   = "myNewCluster"
+    region         = "us-east-1" 
+    instance-types = [ "t2.micro" ] 
+    desired_size   = 1
+    max_size       = 2
+    min_size       = 1
+    max_unaval     = 1
+    cidr_block     = "10.0.0.0/16"
+    dns_enable     = true
+    dns_host       = true
 
-#}
+}
 
 module "dev_vm" {
     source = "./ubuntu_vm"
     instance_type = "t2.medium"
     ami = "ami-0ae9328bcad70db94"
     
-   # region = "us-east-1"
-   # encrypted = true
-   # bucket_name = "truthbucket9846575"
-    #acl         = "public-read-write"
-   # tag_name = "samplebucket"
+    region = "us-east-1"
+    encrypted = true
+    bucket_name = "truthbucket9846575"
+    acl         = "public-read-write"
+    tag_name = "samplebucket"
 
 
 }
